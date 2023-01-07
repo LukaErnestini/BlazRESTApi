86890aadc57c85da7faba610ba0d1ac2e843efd2
Luka Ernestini <luka.ernestini@gmail.com>
2023-01-07 22:03:41 +0100

    disable sequelize logging

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const sequelize = new Sequelize(
   "blaz_db",
   "root",
   "BWBnlNRjU2BAw32yaNpOajnZH",
-  { dialect: "mysql", host: "localhost" }
+  { dialect: "mysql", host: "localhost", logging: false }
 );
 
 module.exports = sequelize;
